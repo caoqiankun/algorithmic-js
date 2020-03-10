e4f7c5ee84820e990d1aaee8bec32701b17acc52
caoqiankun <297410464@qq.com>
2020-03-10 17:55:46 +0800

    提交

--- dataStruAndAlg/js/stack.js
+++ dataStruAndAlg/js/stack.js
@@ -46,3 +46,18 @@ console.log(s.peek());
 s.push("Clayton");
 console.log(s.peek());
 console.log(s.dataStore)
+
+function mulBase(num,base) {
+    var s = new Stack()
+    do{
+        s.push(num%base)
+        num = Math.floor(num/=base)
+    }while(num>0)
+    var converted = ""
+    while(s.length()>0){
+        converted += s.pop()
+    }
+    return converted
+}
+
+console.log(mulBase(12,2))

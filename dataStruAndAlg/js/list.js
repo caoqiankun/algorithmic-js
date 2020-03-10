7421b598131ec865eaba73fd291b208a33b71fea
caoqiankun <297410464@qq.com>
2020-03-10 15:36:22 +0800

    添加insert方法

--- dataStruAndAlg/js/list.js
+++ dataStruAndAlg/js/list.js
@@ -54,6 +54,18 @@ function toString() {
     return this.dataStore
 }
 
+function insert(element,after) {
+    var insertPos = this.find(after)
+    if(insertPos>-1) {
+        this.dataStore.splice(insertPos+1, 0,element)
+        ++this.listSize
+        return true
+    }
+    return false
+}
+
+
+
 var names = new List();
 names.append("Cynthia");
 names.append("Raymond");
